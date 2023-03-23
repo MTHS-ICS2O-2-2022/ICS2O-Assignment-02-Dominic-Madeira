484e38fe3ab5411d0727ab528175edb4de825c2e
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-03-23 17:29:50 +0000

    first edits

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dominic M.
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+* This function calculates the volume of a cube.
+*/
+function myButtonClicked () {
+ // input
+  const edge = parseInt(document.getElementById('side-length').value)
+
+ // process
+  const volume = (edge ** 3)
+
+ // output
+  document.getElementById('volume').innerHTML = 'The Volume of the Square is: ' + volume + ' cm³'
+}
